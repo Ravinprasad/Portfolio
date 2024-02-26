5b0651b0c02a927b5b2ba7bdf0af68bb5a62db00
Ravinprasad Ramachandern <159550648+Ravinprasad@users.noreply.github.com>
2024-02-26 13:52:06 +0800

    Update CaseStudy(NashvileHousing).sql

--- CaseStudy(NashvileHousing).sql
+++ CaseStudy(NashvileHousing).sql
@@ -1,17 +1,22 @@
+--Filling in the empty cells with null 
 UPDATE nashvile
 SET PropertyAddress = NULL 
 WHERE PropertyAddress = '';
 
+--Selecting all the columns from the table
 SELECT * FROM project.nashvile;
 
+--Changing the sale date format 
 SELECT SaleDate FROM project.nashvile;
 
 SELECT DATE_FORMAT(STR_TO_DATE(SaleDate, '%M %d, %Y'), '%m/%d/%Y') 
 FROM project.nashvile;
 
+--Updating the sale date format
 UPDATE nashvile
 SET SaleDate = DATE_FORMAT(STR_TO_DATE(SaleDate, '%M %d, %Y'), '%m/%d/%Y');
 
+--Populating the null values in the property address column using joins
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, IFNULL(a.PropertyAddress, b.PropertyAddress)
 FROM nashvile a
 INNER JOIN nashvile b
@@ -19,6 +24,7 @@ ON a.ParcelID =  b.ParcelID
 AND a.UniqueID <> b.UniqueID
 WHERE a.PropertyAddress IS NULL;
 
+--Splitting the address to make it more usable
 SELECT
 SUBSTRING_INDEX(PropertyAddress, ",", 1) AS Address,
 SUBSTRING_INDEX(PropertyAddress, ",", -1) AS City
@@ -34,6 +40,7 @@ SET SplitAdress = SUBSTRING_INDEX(PropertyAddress, ",", 1);
 UPDATE nashvile
 SET SplitCity = SUBSTRING_INDEX(PropertyAddress, ",", -1);
 
+--Changing the sold as vacant column into a more standardized format of Yes or No
 SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
 FROM project.nashvile
 GROUP BY SoldAsVacant
@@ -52,7 +59,7 @@ WHEN SoldAsVacant = 'N' THEN 'No'
 ELSE SoldAsVacant
 END;
 
-
+--Finding out any duplicate columns
 SELECT ParcelID FROM (
 SELECT ParcelID,
   ROW_NUMBER() OVER (
